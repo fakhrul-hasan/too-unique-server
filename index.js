82f0abae24e3824f515345ad360e8c65dd816fc4
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-05-19 01:17:27 +0600

    data post to mongodb successfully

--- index.js
+++ index.js
@@ -24,12 +24,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toysCollection = client.db('tooUniqueDB').collection('toys');
+
+    app.post('/addToy', async(req,res)=>{
+        const toy = req.body;
+        const result = await toysCollection.insertOne(toy);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
