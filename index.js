39b62ed598cddeacfa3addce87c906677edf191e
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-05-19 17:35:11 +0600

    single toy get from db

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xyrtm8p.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,6 +54,11 @@ async function run() {
         const result = await toysCollection.find({subCategoryName: 'Monster Trucks'}).toArray();
         return res.send(result);
       }
+    })
+    app.get('/toyDetails/:id', async(req,res)=>{
+      const id = req.params.id;
+      const result = await toysCollection.find({_id: new ObjectId(id)}).toArray();
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
